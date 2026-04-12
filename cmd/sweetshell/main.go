3ecafc4b152771a1c4f353848ff7ff79eb7b8372
rexlx <rxlx@nullferatu.com>
2026-04-12 09:24:00 -0500

    bn

--- cmd/sweetshell/main.go
+++ cmd/sweetshell/main.go
@@ -9,7 +9,11 @@ import (
 )
 
 func main() {
-	internal.InitDB("user=rxlx password=thereISnosp0)n host=192.168.86.120 dbname=sweetshell")
+	connStr := "postgres://rxlx:thereISnosp0)n@192.168.86.120:5432/sweetshell?sslmode=disable"
+	err := internal.InitDB(connStr)
+	if err != nil {
+		panic(err)
+	}
 	app := internal.NewApp("my-secret-key", 24*time.Hour)
 
 	sshPot := sshhoneypot.NewSSHHoneypot(2222)
